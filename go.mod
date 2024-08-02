ff2d2d036dbb53d57a8a574370a3b77522d6551b
moaml <63509020+moamlrh@users.noreply.github.com>
2024-08-02 16:28:03 +0300

    init port scanner

---
+++ go.mod
@@ -0,0 +1,5 @@
+module portscanner
+
+go 1.22.5
+
+require golang.org/x/time v0.5.0
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+type PortScanner struct {
+	ip      string
+	workers int
+}
+
+func ScanPort(ctx context.Context, ip string, port int, timeout time.Duration) bool {
+	target := fmt.Sprintf("%s:%d", ip, port)
+	conn, err := net.DialTimeout("tcp", target, timeout)
+	if err == nil {
+		conn.Close()
+		return true
+	}
+	return false
+}
+
+func (ps *PortScanner) Start(ctx context.Context, startPort, endPort int, timeout time.Duration, rateLimiter *rate.Limiter) {
+	portsChan := make(chan int, ps.workers)
+
+	var wg sync.WaitGroup
+	for i := 0; i < ps.workers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for port := range portsChan {
+				if err := rateLimiter.Wait(ctx); err != nil {
+					log.Printf("Rate limiter error: %v", err)
+					return
+				}
+
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					if ScanPort(ctx, ps.ip, port, timeout) {
+						log.Printf("\033[32mPort %d is open\033[0m", port)
+					}
+				}
+			}
+		}()
+	}
+
+	go func() {
+		defer close(portsChan)
+		for port := startPort; port <= endPort; port++ {
+			select {
+			case <-ctx.Done():
+				return
+			case portsChan <- port:
+			}
+		}
+	}()
+
+	wg.Wait()
+}
+
+func main() {
+	ip := flag.String("ip", "142.250.187.110", "IP address to scan")
+	workers := flag.Int("workers", 250, "Number of concurrent workers")
+	startPort := flag.Int("start", 1, "Start port")
+	endPort := flag.Int("end", 1000, "End port")
+	timeout := flag.Duration("timeout", 500*time.Millisecond, "Timeout for each port scan")
+	rateLimit := flag.Float64("rate", 1000, "Maximum scan rate (ports per second)")
+	flag.Parse()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	go func() {
+		<-signalChan
+		log.Println("Shutting down...")
+		os.Exit(0)
+	}()
+
+	ps := &PortScanner{
+		ip:      *ip,
+		workers: *workers,
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	rateLimiter := rate.NewLimiter(rate.Limit(*rateLimit), 1)
+
+	log.Printf("Scanning ports %d to %d on %s...\n", *startPort, *endPort, *ip)
+	ps.Start(ctx, *startPort, *endPort, *timeout, rateLimiter)
+	log.Printf("Scanning completed")
+}
